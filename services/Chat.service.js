831b6ffec9b097744cece88ab8b37337a359561a
manoj <manojpawane@gmail.com>
2018-11-29 12:06:03 +0530

    chat app single node completed

--- services/Chat.service.js
+++ services/Chat.service.js
@@ -7,6 +7,12 @@ exports.chat_singleChat = function(req, res){
         var d = new Date();
         res.send(d);
     }
+    else if(question === 'what is computer'){
+         res.send('A computer is a machine or device that performs processes, calculations and operations.')
+    }
+    else if(question === 'what is internet'){
+        res.send('a global computer network providing a variety of information and communication facilities, consisting of interconnected networks using standardized communication protocols.');
+    }
     else{
         res.send('Sorry, please i am not upto')
     }
